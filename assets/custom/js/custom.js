ad905232b7d350e7289567a7e4870f1219f416dd
Guilherme Dio <ggrdio@gmail.com>
2016-06-04 21:02:02 -0300

    Configured google maps and removed custom style modifier

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -416,7 +416,7 @@
 
                 // Create a map object, and include the MapTypeId to add
                 // to the map type control.
-                var $latlng = new google.maps.LatLng(52.5075419, 13.4261419),
+                var $latlng = new google.maps.LatLng(-23.605356, -46.694064),
                     $mapOptions = {
                         zoom: 13,
                         center: $latlng,
